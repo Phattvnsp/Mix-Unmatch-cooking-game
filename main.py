4e7370a46aa7f459288e5fe888c27a28f023c598
Nemmno <67991005@kmitl.ac.th>
2026-02-11 07:44:41 +0700

    add beef

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class KitchenScene(Scene):
         # Make sure you have .png images for all of these!
         shelf_list = [
             "Egg", "Cheese", "Rice", "Fish", "Seaweed", "Tobiko",
-            "Chicken", "Pork"
+            "Chicken", "Pork", "Beef"
         ]
         
         # Grid Settings
